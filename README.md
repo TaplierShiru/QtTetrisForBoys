3de255b523674b120be02d326186cc1d73da41c4
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-08-25 22:24:44 +0400

    add README.md, update( main_window/menu/menu): header in menu

---
+++ README.md
@@ -0,0 +1,16 @@
+### QtTetrisForBoys
+
+This repo is simple Tetris application written with Qt framework. In my case, I wrote it for improve knowledge in Qt 
+framework. I hope you find it useful for your future code examples of just find it funny.
+
+## Setup
+
+Here will be some library which is need to install
+
+...soon...
+
+
+## How to start
+
+Call `main.py` to begin game
+
--- main_window/menu/menu.py
+++ main_window/menu/menu.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor, QPalette
-from PyQt5.QtWidgets import (QGridLayout,
+from PyQt5.QtGui import QColor, QPalette, QFont
+from PyQt5.QtWidgets import (QVBoxLayout, QLabel, QFrame,
                              QPushButton, QWidget,
                              QDesktopWidget, QApplication,
                              )
@@ -18,15 +18,24 @@ class MainMenu(QWidget):
         Init app
 
         """
-        grid = QGridLayout()
+        vbox = QVBoxLayout()
+
+        # Label with name of the game (i.e. Tetris in our case)
+        self._label_widget = QLabel("Tetris")
+        self._label_widget.setFont(QFont('Times', 20, QFont.Bold))
+        self._label_widget.setMaximumHeight(50)
+        self._label_widget.setFrameStyle(QFrame.Panel)
+        self._label_widget.setStyleSheet("background-color: rgb(200, 160, 160)")
+        self._label_widget.setAlignment(Qt.AlignCenter)
+        vbox.addWidget(self._label_widget)
 
         # Create button and widget for game
-        self._game_button  = QPushButton("Play game")
+        self._game_button = QPushButton("Play game")
         pal = self._game_button.palette()
         pal.setColor(QPalette.Button, QColor(Qt.green))
         self._game_button.setAutoFillBackground(True)
         self._game_button.setPalette(pal)
-        grid.addWidget(self._game_button, 0, 0)
+        vbox.addWidget(self._game_button)
 
 
         # Create button and widget for settings
@@ -35,7 +44,7 @@ class MainMenu(QWidget):
         pal.setColor(QPalette.Button, QColor(Qt.blue))
         self._settings_button.setAutoFillBackground(True)
         self._settings_button.setPalette(pal)
-        grid.addWidget(self._settings_button, 1, 0)
+        vbox.addWidget(self._settings_button)
 
 
         # Create button and widget about_game
@@ -44,8 +53,7 @@ class MainMenu(QWidget):
         pal.setColor(QPalette.Button, QColor(Qt.yellow))
         self._about_game_button.setAutoFillBackground(True)
         self._about_game_button.setPalette(pal)
-        
-        grid.addWidget(self._about_game_button, 2, 0)
+        vbox.addWidget(self._about_game_button)
 
         # Create quit button
         self._exit_button = QPushButton("Exit")
@@ -54,24 +62,10 @@ class MainMenu(QWidget):
         self._exit_button.setAutoFillBackground(True)
         self._exit_button.setPalette(pal)
         self._exit_button.clicked.connect(QApplication.quit)
+        vbox.addWidget(self._exit_button)
 
-        grid.addWidget(self._exit_button, 3, 0)
-
-        self.setLayout(grid)
+        self.setLayout(vbox)
         self.setWindowTitle("Main menu")
-    
-    def center(self):
-        """
-        Centers the window on the screen
-
-        """
-
-        screen = QDesktopWidget().screenGeometry()
-        size = self.geometry()
-        self.move(
-            int( (screen.width() - size.width()) / 2),
-            int( (screen.height() - size.height()) / 2)
-        )
 
     def connect_setting_button(self, connect_to):
         """
